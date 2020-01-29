1699ce97663eccf4c68f48639c4b34421fb3d617
Indra Gunawan <indragunawanxiaopingping@gmmail.com>
2020-01-29 19:48:29 +0700

    Second Commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA3cqlX98FGQjUoGI042KABJ8KZH_FFiuQ",
+    authDomain: "resepku-f44bb.firebaseapp.com",
+    databaseURL: "https://resepku-f44bb.firebaseio.com",
+    projectId: "resepku-f44bb",
+    storageBucket: "resepku-f44bb.appspot.com",
+    messagingSenderId: "83469598345",
+    appId: "1:83469598345:web:9fab0cbb7b8eb5dea99058",
+    measurementId: "G-5N5RFCZ62C"
+  }
 };
 
 /*
