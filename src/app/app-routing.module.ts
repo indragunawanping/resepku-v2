5942e3471447ff5f7212b5a946f32c08e26773ac
Indra Gunawan <indragunawanxiaopingping@gmmail.com>
2020-01-29 17:21:16 +0700

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,38 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'tabs', pathMatch: 'full' },
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'credits', loadChildren: './credits/credits.module#CreditsPageModule' },
+  { path: 'tabs/beranda/:menuType', loadChildren: './menu/menus/menus.module#MenusPageModule' },
+  { path: 'tabs/beranda/:menuType/:menuId', loadChildren: './menu/menu-detail/menu-detail.module#MenuDetailPageModule' }
+
+  // { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  // {
+  //   path: 'beranda',
+  //   loadChildren: () => import('./tabs/beranda/beranda.module').then( m => m.BerandaPageModule)
+  // },
+  // {
+  //   path: 'bookmark',
+  //   loadChildren: () => import('./tabs/bookmark/bookmark.module').then( m => m.BookmarkPageModule)
+  // },
+  // {
+  //   path: 'history',
+  //   loadChildren: () => import('./tabs/history/history.module').then( m => m.HistoryPageModule)
+  // },
+  // {
+  //   path: 'menus',
+  //   loadChildren: () => import('./menu/menus/menus.module').then( m => m.MenusPageModule)
+  // },
+  // {
+  //   path: 'menu-detail',
+  //   loadChildren: () => import('./menu/menu-detail/menu-detail.module').then( m => m.MenuDetailPageModule)
+  // },
+  // {
+  //   path: 'credits',
+  //   loadChildren: () => import('./credits/credits.module').then( m => m.CreditsPageModule)
+  // },
 ];
 
 @NgModule({
---
+++ src/app/tabs/tabs-routing.module.ts
@@ -0,0 +1,56 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsPage,
+    children: [
+        {
+            path:  'beranda',
+            children: [
+                {
+                    path: '',
+                    loadChildren: './beranda/beranda.module#BerandaPageModule'
+                },
+            ]
+        },
+        {
+            path: 'history',
+            children: [
+                {
+                    path: '',
+                    loadChildren: './history/history.module#HistoryPageModule'
+                }
+            ]
+        },
+        {
+            path: 'bookmark',
+            children: [
+                {
+                    path: '',
+                    loadChildren: './bookmark/bookmark.module#BookmarkPageModule'
+                }
+            ]
+        },
+        {
+            path: '',
+            redirectTo: '/tabs/beranda',
+            pathMatch: 'full'
+        }
+    ]
+},
+{
+    path: '',
+    redirectTo: '/tabs/beranda',
+    pathMatch: 'full'
+},
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
